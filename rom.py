0ee455f591c490d530d439e781b2fb740723fa10
Aromakelapa <gmrom1404@gmail.com>
2022-06-23 11:48:55 +0700

    Update n Fix Bug

--- rom.py
+++ rom.py
@@ -5,13 +5,15 @@ import os
 
 
 url = input('\nURL Facebook Video : ')
+session = input('Cookie Facebook : ')
+cookie = {'cookie' : f'{session}'}
 # print(rUrl)
 
 print('\nSilahkan tunggu..')
 sleep(1)
 
 rUrl = url.replace('www', 'mbasic', 1)
-r = requests.get(rUrl)
+r = requests.get(rUrl, cookies=cookie)
 
 print('\nStatus : ', r.status_code)
 print()
@@ -29,8 +31,8 @@ with open('lib/index.html', 'w', encoding = 'UTF-8') as file:
 index = open('lib/index.html').read()
 scrap = BeautifulSoup(index, 'html.parser')#.div['ca']
 
-linkk = scrap.find('div', 'ca')
-src = scrap.find('a')['href']
+linkk = scrap.find('div', 'bz')
+src = linkk.find('a')['href']
 fSrc = 'https://mbasic.facebook.com' + src
 sleep(1)
 print('Video Link :\n', fSrc)
